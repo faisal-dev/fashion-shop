eda34aecbeb2fce1595f763fdca6de976e26f72b
faisal-dev <faisal.jeaumar@gmail.com>
2015-11-14 02:40:45 +0700

    Add pager loader script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+;(function($) {
+  'use strict'
+
+  $(document).ready(function() {
+
+    // Pageloader controller
+    setTimeout(function() {
+      $('#pageloader').fadeOut('slow', function() {
+        $(this).addClass('.hidden');
+      });
+    }, 5000);
+
+  });
+
+})(jQuery);
