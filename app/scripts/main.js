ff53873387f810fcea89c7efcf4d3cbb452e87a9
faisal-dev <faisal.jeaumar@gmail.com>
2015-11-14 19:08:15 +0700

    Add progress bar to slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -30,13 +30,80 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
   $(document).ready(function() {
 
     // Owl banner slider controller
-    var owl = $("#owl-banner");
+    var
+      owl = $('#owl-banner'),
+      time = 7, // time in seconds
 
+      $progressBar,
+      $bar,
+      $elem,
+      isPause,
+      tick,
+      percentTime;
+
+    //Init the carousel
     owl.owlCarousel({
-      navigation : true,
       singleItem : true,
-      transitionStyle : "fade"
+      transitionStyle : 'fade',
+      afterInit : progressBar,
+      afterMove : moved,
+      startDragging : pauseOnDragging
     });
+
+    //Init progressBar where elem is $('#owl-banner')
+    function progressBar(elem){
+      $elem = elem;
+      //build progress bar elements
+      buildProgressBar();
+      //start counting
+      start();
+    }
+
+    //create div#progressBar and div#bar then prepend to $('#owl-banner')
+    function buildProgressBar(){
+      $progressBar = $('<div>',{
+        id:'progressBar'
+      });
+      $bar = $('<div>',{
+        id:'bar'
+      });
+      $progressBar.append($bar).prependTo($elem);
+    }
+
+    function start() {
+      //reset timer
+      percentTime = 0;
+      isPause = false;
+      //run interval every 0.01 second
+      tick = setInterval(interval, 10);
+    }
+
+    function interval() {
+      if(isPause === false){
+        percentTime += 1 / time;
+        $bar.css({
+           width: percentTime+'%'
+         });
+        //if percentTime is equal or greater than 100
+        if(percentTime >= 100){
+          //slide to next item
+          $elem.trigger('owl.next')
+        }
+      }
+    }
+
+    //pause while dragging
+    function pauseOnDragging(){
+      isPause = true;
+    }
+
+    //moved callback
+    function moved(){
+      //clear interval
+      clearTimeout(tick);
+      //start again
+      start();
+    }
 
   });
 
