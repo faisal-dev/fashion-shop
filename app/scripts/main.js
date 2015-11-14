998d80543b90fc1d38a708f228c9049fd86ac2e8
faisal-dev <faisal.jeaumar@gmail.com>
2015-11-14 20:49:01 +0700

    Add mappy-breakpoint mixin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -43,11 +43,13 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 
     //Init the carousel
     owl.owlCarousel({
+      autoPlay : false,
+      pagination : false,
       singleItem : true,
-      transitionStyle : 'fade',
-      afterInit : progressBar,
-      afterMove : moved,
-      startDragging : pauseOnDragging
+      // transitionStyle : 'fade',
+      // afterInit : progressBar,
+      // afterMove : moved,
+      // startDragging : pauseOnDragging
     });
 
     //Init progressBar where elem is $('#owl-banner')
