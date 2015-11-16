d23305218e182f8c2eeeb11b2a13775db0a84c6e
faisal-dev <faisal.jeaumar@gmail.com>
2015-11-16 09:47:28 +0700

    Add thumb image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,29 +3,29 @@ console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 ;(function($) {
   'use strict'
 
-  $(window).load(function() {
-    // Simulate fake loader page
-    var
-      pageloader = $('#pageloader'),
-      svgElem = $('dc-spinner'),
-      body = $('#shop-theme');
+  // $(window).load(function() {
+  //   // Simulate fake loader page
+  //   var
+  //     pageloader = $('#pageloader'),
+  //     svgElem = $('dc-spinner'),
+  //     body = $('#shop-theme');
 
-    setTimeout(function() {
-      svgElem.attr('class', 'svgloader _fade-out');  // SVG don't have addClass().
+  //   setTimeout(function() {
+  //     svgElem.attr('class', 'svgloader _fade-out');  // SVG don't have addClass().
 
-      setTimeout(function() {
-        pageloader
-          .addClass('_fade-out')
-          .bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function() {
-            // hiding block when css animation finished.
-            $(this).addClass('hidden');
-            // Set overflow-y auto;
-            body.addClass('_loaded');
-          });
-      }, 500);
+  //     setTimeout(function() {
+  //       pageloader
+  //         .addClass('_fade-out')
+  //         .bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function() {
+  //           // hiding block when css animation finished.
+  //           $(this).addClass('hidden');
+  //           // Set overflow-y auto;
+  //           body.addClass('_loaded');
+  //         });
+  //     }, 500);
 
-    }, 5000);
-  });
+  //   }, 5000);
+  // });
 
   $(document).ready(function() {
 
