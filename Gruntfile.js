7b0e3abe3565566837339c094bbedd1c25730de8
faisal-dev <faisal.jeaumar@gmail.com>
2015-11-18 12:05:57 +0700

    Change preloading

--- Gruntfile.js
+++ Gruntfile.js
@@ -335,6 +335,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
+            'fonts/{,*/}*.*',
             '{,*/}*.html',
             'styles/fonts/{,*/}*.*'
           ]
